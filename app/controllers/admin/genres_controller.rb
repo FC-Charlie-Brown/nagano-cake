eb8b700e33624df75df29b678b3813fb90084dd5
EC2 Default User <ec2-user@ip-172-31-73-194.ec2.internal>
2021-03-18 06:06:07 +0000

    ジャンル登録機能追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,5 +1,8 @@
 class Admin::GenresController < ApplicationController
+  
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def update
@@ -9,5 +12,13 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to admin_genres_path
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
