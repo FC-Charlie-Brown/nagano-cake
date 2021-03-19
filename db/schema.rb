a81cf6c8d00aa54a58a5c262cad0109b0625dc1e
70ki8suda <telekinesick@gmail.com>
2021-03-19 16:18:14 +0900

    会員まわりのstyle調整

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2021_03_16_074103) do
   create_table "cart_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "customer_id", null: false
     t.integer "item_id", null: false
-    t.integer "amount", null: false
+    t.integer "amount", null: falsegit
+    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
     t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
