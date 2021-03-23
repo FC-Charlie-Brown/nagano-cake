28d6d38c1104c4aeedbe8ead490c735413e3dacb
nakamurammm <mr.mar2.sor@gmail.com>
2021-03-23 02:41:08 +0000

    管理者ログアウト後のリダイレクト先変更

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -23,17 +23,44 @@ class Public::OrdersController < ApplicationController
     @order.save
     redirect_to confirm_path
   end
+  
+  def create_new_address
+    @new_address = Address.new(address_params)
+    @new_address.customer_id = current_customer.id
+    @new_address.save
+    redirect_to confirm_path
+  end
+
 
 
   def confirm
-    # @cart_item = current_cartとか？？
+    @cart_items = CartItem.where(customer_id: current_customer.id)
     @addresses = Address.where(customer_id: current_customer.id)
     @orders = Order.where(customer_id: current_customer.id)
     @order = Order.new(
       customer: current_customer,
-      payment_way: params[:order][:payment_way]
+      payment_way: params[:order][:payment_way],
+      address: params[:order][:address]
     )
+    
+    if params[:order][:address] == 0
+      
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + current_customer.first_name
+    
+    elsif params[:order][:address] == 1
+    
+    elsif params[:order][:address] == 2
+    @nfew_address = Address.new(address_params)
+    @new_address.customer_id = current_customer.id
+      @new_address.save
+      redirect_to confirm_path
+     
+    end
+
   end
+
 
   def complete
   end
