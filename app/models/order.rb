34523a1fd2d3772d279937deb8b7c733a2b1e919
nakamurammm <mr.mar2.sor@gmail.com>
2021-03-25 05:43:42 +0000

    finish

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
     has_many :order_details
     enum payment_way:{ card: 0, bank: 1 }
 
-    enum order_status: { wait: 0, confirm: 1, making: 2, shipping: 3, shipped: 4 }
+    enum order_status:{ wait: 0, confirm: 1, making: 2, shipping: 3, shipped: 4 }
 
 
 end
