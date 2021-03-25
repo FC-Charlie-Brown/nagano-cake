23faa046808109857660fd288da2399fe179d742
nakamurammm <mr.mar2.sor@gmail.com>
2021-03-25 03:29:16 +0000

    add-design-sign-in-up

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
     has_many :order_details
     enum payment_way:{ card: 0, bank: 1 }
 
-    enum orders_status: { 入金待ち: 0,入金確認: 1,製作中: 2,発送準備中: 3, 発送済み: 4}
+    enum order_status: { wait: 0, confirm: 1, making: 2, shipping: 3, shipped: 4 }
 
 
 end
